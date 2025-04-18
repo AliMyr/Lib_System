db7847672786498b82ebed92c8f2177d22634f0e
AliMyr <rabo41ali@gmail.com>
2025-04-18 16:08:12 +0500

    (TEST) Log Session Edit

--- Services/Interfaces/LogSessionService/ILogSessionService.cs
+++ Services/Interfaces/LogSessionService/ILogSessionService.cs
@@ -6,5 +6,8 @@ namespace Lib_System.Services.Interfaces
     public interface ILogSessionService
     {
         IEnumerable<LogSession> GetAllSessions();
+        ulong CreateSession(LogSession session);
+        bool UpdateSession(LogSession session);
+        bool DeleteSession(ulong id);
     }
 }
--- Views/LogSessionsWindow.xaml.cs
+++ Views/LogSessionsWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using Lib_System.Models;
 using Lib_System.Services;
 using Lib_System.Services.Interfaces;
 
@@ -6,11 +8,39 @@ namespace Lib_System.Views
 {
     public partial class LogSessionsWindow : Window
     {
+        private readonly ILogSessionService _svc;
         public LogSessionsWindow()
         {
             InitializeComponent();
-            var svc = new LogSessionService(new DbService());
-            SessionsGrid.ItemsSource = svc.GetAllSessions();
+            _svc = new LogSessionService(new DbService());
+            Load();
+        }
+
+        private void Load() => SessionsGrid.ItemsSource = _svc.GetAllSessions().ToList();
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            var win = new LogSessionEditWindow(_svc);
+            if (win.ShowDialog() == true) Load();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (SessionsGrid.SelectedItem is LogSession s)
+            {
+                var win = new LogSessionEditWindow(_svc, s);
+                if (win.ShowDialog() == true) Load();
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (SessionsGrid.SelectedItem is LogSession s &&
+                MessageBox.Show("Delete?", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                _svc.DeleteSession(s.Id);
+                Load();
+            }
         }
     }
 }
