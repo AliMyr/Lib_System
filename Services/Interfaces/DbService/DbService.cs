d804abfa94d2931cc9ffe43f10152297edfbc184
AliMyr <rabo41ali@gmail.com>
2025-04-24 13:11:22 +0500

    (TEST) Remote Server
    В App.config вставляете данные своей базы/хоста

--- Services/Interfaces/DbService/DbService.cs
+++ Services/Interfaces/DbService/DbService.cs
@@ -10,7 +10,8 @@ namespace Lib_System.Services
         private readonly string _connectionString;
         public DbService()
         {
-            _connectionString = ConfigurationManager.ConnectionStrings["LocalMySqlConnection"].ConnectionString;
+            //_connectionString = ConfigurationManager.ConnectionStrings["LocalMySqlConnection"].ConnectionString;
+            _connectionString = ConfigurationManager.ConnectionStrings["DefaultMySql"].ConnectionString;
         }
 
         public IDbConnection GetConnection() => new MySqlConnection(_connectionString);
